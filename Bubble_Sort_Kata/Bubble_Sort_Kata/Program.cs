8ffdc328afc5ad2f61c9d078e708d6c8fcb9372c
Andrew Ma <ma.andrew2984@gmail.com>
2023-04-12 16:48:48 +0100

    Updated SortKata

--- Bubble_Sort_Kata/Bubble_Sort_Kata/Program.cs
+++ Bubble_Sort_Kata/Bubble_Sort_Kata/Program.cs
@@ -1,5 +1,5 @@
 ﻿namespace BubbleSortKata { 
-    public class BubbleSortClass {
+    public class KataSortClass {
         static void Main()
         {
             //procedure bubbleSort(A : list of sortable items)
@@ -20,7 +20,62 @@
 
         public static int[] BubbleSort(int[] inputArray)
         {
+            int n = inputArray.Length;
+            bool swapped = false;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i <= n - 1; i++)
+                {
+                    if (inputArray[i-1] > inputArray[i])
+                    {
+                        int a = inputArray[i - 1];
+                        inputArray[i - 1] = inputArray[i];
+                        inputArray[i] = a;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
             return inputArray;
+        }
+
+        public static int[] MergeSort(int[] input1, int[] input2)
+        {
+            if (input1 is null || input2 is null) throw new ArgumentException("Null");
+            if (input1.Length == 0) return input2;
+            if (input2.Length == 0) return input1;
+            int[] result = new int[input1.Length + input2.Length];
+            int pointer = 0;
+            int index1 = 0;
+            int index2 = 0;
+            while (index1 < input1.Length && index2 < input2.Length)
+            {
+                if (input1[index1] < input2[index2])
+                {
+                    result[pointer] = input1[index1];
+                    index1++;
+                }
+                else
+                {
+                    result[pointer] = input2[index2];
+                    index2++;
+                }
+                pointer++;
+            }
+            while (index1 < input1.Length)
+            {
+                result[pointer] = input1[index1];
+                index1++;
+                pointer++;
+            }
+            while (index2 < input2.Length)
+            {
+                result[pointer] = input2[index2];
+                index2++;
+                pointer++;
+            }
+
+            return result;
         }
     }
 }
--- Bubble_Sort_Kata/Tests/UnitTest1.cs
+++ Bubble_Sort_Kata/Tests/UnitTest1.cs
@@ -5,18 +5,94 @@ namespace Tests
 {
     public class Tests
     {
-
-        [Test]
-        public void GivenAnEmptyArrayReturnsEmptyArray()
+        [TestFixture]
+        public class BubbleSort
         {
-            //Arrange
-            var testArray = new int[] { };
+            [Test]
+            public void GivenAnEmptyArrayReturnsEmptyArray()
+            {
+                //Arrange
+                var testArray = new int[] { };
 
-            //Act
-            var output = BubbleSortClass.BubbleSort(testArray);
+                //Act
+                var output = KataSortClass.BubbleSort(testArray);
 
-            //Assert
-            Assert.That(output, Is.EqualTo(testArray));
+                //Assert
+                Assert.That(output, Is.EqualTo(testArray));
+            }
+
+            [Test]
+
+            public void GivenAnArrayWithNElements_ReturnsArrayWithSameLength()
+            {
+                var testArray = new int[] { 1, 2, 3 };
+                var output = KataSortClass.BubbleSort(testArray).Length;
+                Assert.That(output, Is.EqualTo(3));
+            }
+
+            [Test]
+
+            public void GivenAnArrayWithNElements_ReturnsArrayWithSameArraySortedLowToHigh()
+            {
+                var testArray = new int[] { 5, 1, -3, 3, 6 };
+                var output = KataSortClass.BubbleSort(testArray);
+                Assert.That(output, Is.EqualTo(new int[] { -3, 1, 3, 5, 6}));
+            }
         }
+        [TestFixture]
+        public class MergeSort
+        {
+            [Test]
+            public void GivenEmptyArrays_ReturnEmptyArray()
+            {
+                var testArray1 = new int[] { };
+                var testArray2 = new int[] { };
+                var output = KataSortClass.MergeSort(testArray1, testArray2);
+                Assert.That(output, Is.EqualTo(new int[] { }));
+            }
+
+            [Test]
+            public void GivenTwoArrays_IfOneIsEmpty_ReturnsArrayWithLengthOfOther()
+            {
+                var testArray1 = new int[] {1, 2, 3 };
+                var testArray2 = new int[] { };
+                var output = KataSortClass.MergeSort(testArray1, testArray2).Length;
+                Assert.That(output, Is.EqualTo(3));
+            }
+
+            [Test]
+            public void GivenTwoArrays_ReturnArrayWithCombinedLengthOfTwoInputs()
+            {
+                var testArray1 = new int[] { 1, 2, 3 };
+                var testArray2 = new int[] { 1};
+                var output = KataSortClass.MergeSort(testArray1, testArray2).Length;
+                Assert.That(output, Is.EqualTo(4));
+            }
+
+            [Test]
+            public void GivenTwoArrays_ReturnMergedArraySortedInAscendingOrder()
+            {
+                var testArray1 = new int[] { -2, -1, 4 };
+                var testArray2 = new int[] { -1, 4, 10 };
+                var output = KataSortClass.MergeSort(testArray1, testArray2);
+                Assert.That(output, Is.EqualTo(new int[] {-2, -1, -1, 4, 4, 10}));
+            }
+            [Test]
+            public void GivenTwoArraysOfDifferentLength_ReturnMergedArraySorted()
+            {
+                var testArray1 = new int[] { -2, 5};
+                var testArray2 = new int[] { -1, 4, 4, 10, 50 };
+                var output = KataSortClass.MergeSort(testArray1, testArray2);
+                Assert.That(output, Is.EqualTo(new int[] {-2, -1, 4, 4, 5, 10, 50 }));
+            }
+            [Test]
+            public void GivenANullArray_ThrowsException()
+            {
+                var testArray1 = new int[] { 1, 2, 3 };
+                Assert.That(() => KataSortClass.MergeSort(testArray1, null), Throws.TypeOf<ArgumentException>());
+            }
+        }
+
     }
+
 }
